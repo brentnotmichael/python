9b3bf68f80cb63b75ff02a57509d9b4afa7d6880
brentnotmichael <gannettajr@gmail.com>
2020-12-20 13:00:54 -0800

    moved draw_grid out of functions.py and into draw_grid.py

--- thinkpython/functions.py
+++ thinkpython/functions.py
@@ -19,24 +19,4 @@ right_justify("There is no try", intMargin)
 right_justify("-- Yoda", intMargin)
 
 
-def draw_grid(intRows,intCols,intCellWidth,intCellHeight):
-    for row in range(0,intRows):
-        draw_grid_hline(intCols, intCellWidth)
-        draw_grid_hspace(intCols, intCellWidth, intCellHeight)
-    draw_grid_hline(intCols, intCellWidth)
-
-def draw_grid_hline(intCols, intCellWidth):
-    for i in range(0, intCols):
-        print("+" + "-" * intCellWidth*2, end="")
-    print("+")
-
-def draw_grid_hspace(intCols, intCellWidth, intCellHeight):
-    for height in range(0, intCellHeight):
-        for width in range(0,intCols):
-            print("|" + " " * intCellWidth*2, end="")
-        print("|")
-
-# rows, cols, colwidth, colheight
-draw_grid(2,5,3,2)
-
 
